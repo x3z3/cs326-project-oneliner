1f6120b6036b60635f9c93087004b4dfc81d8b1d
Felicia DiPietro <95667697+feliciadipi@users.noreply.github.com>
2022-08-22 23:18:48 -0400

    add APIs

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import users from './users.js';
 import auth from './auth.js';
 import { quote } from './quote.js'
 import { weather } from './weather.js';
+import * as crud from './crud.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(dirname(__filename));
@@ -52,7 +53,6 @@ app.get('/quote', async(req, res) => {
     res.status(200).json(newQuote);
 });
 
-
 app.post('/login', auth.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.status(200).json({status: 'success'});
 });
@@ -66,6 +66,66 @@ app.post('/register', async(req, res) => {
     } else {
         res.status(400).json({status: 'user exists'});
     }
+});
+
+// Felicia
+app.post('/notes/save', async function(req, res) {
+  try {
+    await client.connect();
+    const collection = await client.db('onelinerDB').collection('notes');
+    console.log("Connected to: " + collection.namespace);
+    await crud.saveEntry(req.body.date, req.body.content, collection);
+    res.status(200).send("Successfully saved notes");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Error saving notes");
+  }
+  await client.close();
+});
+
+app.post('/notes/delete', async function(req, res) {
+  try {
+    await client.connect();
+    const collection = await client.db('onelinerDB').collection('notes');
+    console.log("Connected to: " + collection.namespace);
+    await crud.deleteEntry(req.body.date, collection);
+    res.status(200).send("Successfully deleted notes");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Error deleting notes");
+  }
+  await client.close();
+  console.log('DB closed...');
+});
+
+
+app.post('/tasks/save', async function(req, res) {
+  try {
+    await client.connect();
+    const collection = await client.db('onelinerDB').collection('tasks');
+    console.log("Connected to: " + collection.namespace);
+    await crud.saveEntry(req.body.date, req.body.content, collection);
+    res.status(200).send("Successfully saved tasks");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Error saving tasks");
+  }
+  await client.close();
+});
+
+app.post('/tasks/delete', async function(req, res) {
+  try {
+    await client.connect();
+    const collection = await client.db('onelinerDB').collection('tasks');
+    console.log("Connected to: " + collection.namespace);
+    await crud.deleteEntry(req.body.date, collection);
+    res.status(200).send("Successfully deleted tasks");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Error deleting tasks");
+  }
+  await client.close();
+  console.log('DB closed...');
 });
 
 app.listen(port, () => {
